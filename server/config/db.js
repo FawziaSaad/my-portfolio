73d1b7ee6349bd2a0bfcd862836928759f5105e9
FawziaSaad <fawziaanime@gmail.com>
2023-06-15 23:10:35 -0400

    Updated db URI to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -7,5 +7,5 @@
 
 module.exports =
 {
-    "URI": "mongodb://localhost/my_portfolio"
+    "URI": "mongodb+srv://fawziaanime:OjzrIEfyCWjA2UIv@mongodbserver.1gtgeoc.mongodb.net/my_portfolio?retryWrites=true&w=majority"
 }
